4322f7e8248b24f403f92f00beaaa3390a2aae5f
benquang <thangfacebook1905@gmail.com>
2022-11-24 11:57:02 +0700

    n commit

---
+++ Nhom19/Business/Variation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhom19.Business
+{
+    public class Variation
+    {
+        private String variation_id;
+        private int product;
+        private String color;
+        private String size;
+        private int variation_stock;
+        private bool is_active;
+        private DateTime creaded_date;
+
+        public string Variation_id { get => variation_id; set => variation_id = value; }
+        public int Product { get => product; set => product = value; }
+        public string Color { get => color; set => color = value; }
+        public string Size { get => size; set => size = value; }
+        public int Variation_stock { get => variation_stock; set => variation_stock = value; }
+        public bool Is_active { get => is_active; set => is_active = value; }
+        public DateTime Creaded_date { get => creaded_date; set => creaded_date = value; }
+    }
+}
---
+++ Nhom19/Model/ToysDB.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nhom19.Business;
+namespace Nhom19.Model
+{
+    public class ToysDB
+    {
+        public static List<Toys> random6toys()
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection("data source=localhost; database=" + ConnectionP.database + ";User ID = " + ConnectionP.user_id + "; Password = " + ConnectionP.pass);
+                conn.Open();
+
+                SqlCommand cm = new SqlCommand();
+                cm.CommandText = "Random6Toys_Proc";
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Connection = conn;
+
+                //cm.Parameters.Add("@user_id", SqlDbType.NVarChar).Value = user_id;
+
+                List<Toys> toys = new List<Toys>();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Toys toy = new Toys();
+                    toy.Toy_id = Convert.ToInt32(sdr["toy_id"]);
+                    toy.Toy_name = Convert.ToString(sdr["toy_name"]);
+                    toy.Price = Convert.ToDouble(sdr["price"]);
+                    toy.Image = Convert.ToString(sdr["image"]);
+
+                    toys.Add(toy);
+                }
+
+                return toys;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public static Toys getToy(int toy_id)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection("data source=localhost; database=" + ConnectionP.database + ";User ID = " + ConnectionP.user_id + "; Password = " + ConnectionP.pass);
+                conn.Open();
+
+                SqlCommand cm = new SqlCommand();
+                cm.CommandText = "GetToy";
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Connection = conn;
+
+                cm.Parameters.Add("@toy_id", SqlDbType.Int).Value = toy_id;
+
+                Toys toy = new Toys();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    toy.Image = Convert.ToString(sdr["image"]);
+                    toy.Toy_id = Convert.ToInt32(sdr["toy_id"]);
+                    toy.Toy_name = Convert.ToString(sdr["toy_name"]);
+                    toy.Category = Convert.ToString(sdr["category"]);
+                    toy.Price = Convert.ToDouble(sdr["price"]);
+                    toy.Desscription = Convert.ToString(sdr["description"]);
+                }
+
+                return toy;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public static Toys findToy(String tukhoa)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection("data source=localhost; database=" + ConnectionP.database + ";User ID = " + ConnectionP.user_id + "; Password = " + ConnectionP.pass);
+                conn.Open();
+
+                SqlCommand cm = new SqlCommand();
+                cm.CommandText = "FindToy";
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Connection = conn;
+
+                cm.Parameters.Add("@tukhoa", SqlDbType.NVarChar).Value = tukhoa;
+
+                Toys toy = new Toys();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    toy.Toy_id = Convert.ToInt32(sdr["toy_id"]);
+                    toy.Toy_name = Convert.ToString(sdr["toy_name"]);
+                    toy.Price = Convert.ToDouble(sdr["price"]);
+                    toy.Image = Convert.ToString(sdr["image"]);
+                }
+
+                return toy;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
